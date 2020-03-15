199d40a56c7f9136d5b8f0ce349cb4e44190d47b
bikashbhagat01 <bikash.bhagat01@gmail.com>
2020-03-15 19:34:43 +0530

    Added RouteRequestManager

---
+++ src/managers/RouteRequestManager.java
@@ -0,0 +1,29 @@
+package managers;
+
+import assets.Route;
+import assets.RouteRequest;
+
+public class RouteRequestManager {
+  public void create(RouteRequest routeRequest) {
+    // Creates sqlQuery from RouteRequest Object and calls QueryExecutor.executeQuery(sqlQuery)
+  }
+
+  public void update() {
+    // No requirements found
+  }
+
+  public void remove(int routeRequestId) {
+    /*
+    * Create SQLQuery to remove routeRequestId
+    * Call QueryExecutor.executeQuery(sqlQuery)
+    * */
+  }
+
+  public void read() {
+    /*
+     * Create SQL Query to read all info. of table
+     * Create Array of all field names
+     * Call QueryExecutor(sqlQuery, fields[])
+     * */
+  }
+}
